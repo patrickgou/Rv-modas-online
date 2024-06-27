79e79b350c348dccffcd80426e9a47a323ac405b
patrickgou <goulartpatrick5@gmail.com>
2024-06-27 18:51:20 -0300

    first commit

---
+++ js/duvida.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var navList = document.getElementById("navList");
+    if (navList.style.display === "flex") {
+        navList.style.display = "none";
+    } else {
+        navList.style.display = "flex";
+    }
+}
